f1e2d21760137f99533ef162bb97917a4366bd16
Parag Sangtani <61550688+paragsangtani@users.noreply.github.com>
2020-06-23 22:38:56 +0530

    Add files via upload

---
+++ MDK-ARM/SD_card/tm_stm32_fatfs.d
@@ -0,0 +1,4 @@
+sd_card\tm_stm32_fatfs.o: ..\..\..\STM32_LIBRARIES\tm_stm32_fatfs.c
+sd_card\tm_stm32_fatfs.o: ..\..\..\STM32_LIBRARIES\tm_stm32_fatfs.h
+sd_card\tm_stm32_fatfs.o: ..\..\..\STM32_LIBRARIES\stm32fxxx_hal.h
+sd_card\tm_stm32_fatfs.o: C:\Users\parag\OneDrive\Desktop\defines.h
